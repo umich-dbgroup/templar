0387a28f288cc3d58bbaf9161f064acacf478fa5
Chris Baik <chrisjbaik@gmail.com>
2017-06-30 09:17:37 -0400

    Add SchemaTemplateGenerator

---
+++ src/main/java/edu/umich/tbnalir/SchemaTemplateGenerator.java
@@ -0,0 +1,16 @@
+package edu.umich.tbnalir;
+
+/**
+ * Created by cjbaik on 6/30/17.
+ */
+public class SchemaTemplateGenerator {
+    public static void main(String[] args) {
+        // TODO: read in schema from json files
+
+        // TODO: construct predicate/projection templates
+
+        // TODO: generate predicate/projection templates from query log
+
+        // TODO: measure coverage
+    }
+}
