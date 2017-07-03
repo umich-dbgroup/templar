be88e6ba2f119d72213a10096739fa78c7d4b19b
Chris Baik <chrisjbaik@gmail.com>
2017-07-03 13:55:31 -0400

    make Constants lowercase

--- src/main/java/edu/umich/tbnalir/util/Constants.java
+++ src/main/java/edu/umich/tbnalir/util/Constants.java
@@ -4,15 +4,15 @@ package edu.umich.tbnalir.util;
  * Created by cjbaik on 6/30/17.
  */
 public class Constants {
-    public static final String TOP = "#TOP";
-    public static final String PROJ = "#PROJECTION";
-    public static final String PRED = "#PREDICATE";
-    public static final String CMP = "#CMP";
-    public static final String NUM = "#NUM";
-    public static final String STR = "#STR";
-    public static final String DATE = "#DATE";
-    public static final String DATETIME = "#DATETIME";
-    public static final String TIMESTAMP = "#TIMESTAMP";
-    public static final String TIME = "#TIME";
-    public static final String ORDER = "#ORDER";
+    public static final String TOP = "#top";
+    public static final String PROJ = "#projection";
+    public static final String PRED = "#predicate";
+    public static final String CMP = "#cmp";
+    public static final String NUM = "#num";
+    public static final String STR = "#str";
+    public static final String DATE = "#date";
+    public static final String DATETIME = "#datetime";
+    public static final String TIMESTAMP = "#timestamp";
+    public static final String TIME = "#time";
+    public static final String ORDER = "#order";
 }
