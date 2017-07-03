f5170a3f8536cba593853d1d8f64520944906c62
Chris Baik <chrisjbaik@gmail.com>
2017-07-03 13:55:37 -0400

    remove debug log

--- src/main/java/edu/umich/tbnalir/SchemaTemplateGenerator.java
+++ src/main/java/edu/umich/tbnalir/SchemaTemplateGenerator.java
@@ -136,8 +136,10 @@ public class SchemaTemplateGenerator extends TemplateGenerator {
                     continue;
                 }
 
-                // Log.info("FK: " + foreignRelation + "/" + foreignAttribute + "\t"
-                        // + "PK: " + primaryRelation + "/" + primaryAttribute);
+                /*
+                Log.info("FK: " + foreignRelation + "/" + foreignAttribute + "\t"
+                        + "PK: " + primaryRelation + "/" + primaryAttribute);
+                        */
 
                 Set<Attribute> pks = fkpkEdges.get(foreignAttribute);
                 if (pks == null) {
