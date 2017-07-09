5f648745b9deefa2b9d71d8b43df21be96e0846a
Chris Baik <chrisjbaik@gmail.com>
2017-07-08 20:16:18 -0400

    Add scripts for SDSS tests

--- sdss_randomized_test.sh
+++ sdss_randomized_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -a join_levels=("0" "1")
-declare -a log_tmpl=("comp_proj" "comp" "const_proj" "const")
+declare -a log_tmpl=("pred_proj" "pred" "comp_proj" "comp" "const_proj" "const")
 declare -a log_percent=("25" "50")
 
 for jl in "${join_levels[@]}"
@@ -10,7 +10,7 @@ do
     do
         for lp in "${log_percent[@]}"
         do
-            java -Xms64g -Xmx128g -cp build/libs/tb-nalir-all.jar edu.umich.tbnalir.SchemaAndLogTemplateGenerator data/sdss/schema/bestdr7 bestdr7_0.05.parsed $jl $lt $lp 2>&1 | tee results/sdss_randomized/bestdr7_0.05.join${jl}.${lt}.${lp}.out
+            java -Xms64g -Xmx128g -cp build/libs/tb-nalir-all.jar edu.umich.tbnalir.SchemaAndLogTemplateGenerator data/sdss/schema/bestdr7 bestdr7_0.05.parsed $jl $lt $lp random 2>&1 | tee results/sdss_randomized/bestdr7_0.05.join${jl}.${lt}.${lp}.out
         done
     done 
 done
