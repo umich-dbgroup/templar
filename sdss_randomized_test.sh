abe1ff84e78af08cb1d3a44228498e3382cf5108
Chris Baik <chrisjbaik@gmail.com>
2017-07-12 13:56:44 -0400

    Change test scripts

--- sdss_randomized_test.sh
+++ sdss_randomized_test.sh
@@ -2,7 +2,7 @@
 
 declare -a join_levels=("0" "1")
 declare -a log_tmpl=("pred_proj" "pred" "comp_proj" "comp" "const_proj" "const")
-declare -a log_percent=("25" "50")
+declare -a log_amounts=("100" "1000" "10p" "25p" "50p")
 
 for jl in "${join_levels[@]}"
 do
@@ -10,7 +10,7 @@ do
     do
         for lp in "${log_percent[@]}"
         do
-            java -Xms64g -Xmx128g -cp build/libs/tb-nalir-all.jar edu.umich.tbnalir.SchemaAndLogTemplateGenerator data/sdss/schema/bestdr7 bestdr7_0.05.parsed $jl $lt $lp random 2>&1 | tee results/sdss_randomized/bestdr7_0.05.join${jl}.${lt}.${lp}.out
+            java -Xms64g -Xmx128g -cp build/libs/tb-nalir-all.jar edu.umich.tbnalir.SchemaAndLogTemplateGenerator data/sdss/schema/bestdr7 bestdr7_0.05.parsed $jl $lt $la random results/sdss_randomized/bestdr7_0.05.join${jl}.${lt}.${la}.err  2>&1 | tee results/sdss_randomized/bestdr7_0.05.join${jl}.${lt}.${la}.out
         done
     done 
 done
--- sdss_temporal_test.sh
+++ sdss_temporal_test.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-declare -a join_levels=("0" "1")
-declare -a log_tmpl=("pred_proj" "pred" "comp_proj" "comp" "const_proj" "const")
-declare -a log_percent=("01" "05" "10" "25" "50")
+declare -a join_levels=("1")
+# declare -a join_levels=("0" "1")
+declare -a log_tmpl=("pred_proj" "comp_proj" "const_proj")
+# declare -a log_tmpl=("pred_proj" "pred" "comp_proj" "comp" "const_proj" "const")
+declare -a log_amounts=("50p")
+# declare -a log_amounts=("100" "1000" "10p" "25p" "50p")
 
 for jl in "${join_levels[@]}"
 do
     for lt in "${log_tmpl[@]}"
     do
-        for lp in "${log_percent[@]}"
+        for la in "${log_amounts[@]}"
         do
-            java -Xms64g -Xmx128g -cp build/libs/tb-nalir-all.jar edu.umich.tbnalir.SchemaAndLogTemplateGenerator data/sdss/schema/bestdr7 bestdr7_0.05.parsed $jl $lt $lp temporal 2>&1 | tee results/sdss_temporal/bestdr7_0.05.join${jl}.${lt}.${lp}.out
+            java -Xms64g -Xmx128g -cp build/libs/tb-nalir-all.jar edu.umich.tbnalir.SchemaAndLogTemplateGenerator data/sdss/schema/bestdr7 bestdr7_0.05.parsed $jl $lt $la temporal results/sdss_temporal/bestdr7_0.05.join${jl}.${lt}.${la}.err  2>&1 | tee results/sdss_temporal/bestdr7_0.05.join${jl}.${lt}.${la}.out
         done
     done 
 done
