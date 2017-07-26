cb465f3e4c0ac04d0b8e113b927409e3251cdc4f
Chris Baik <chrisjbaik@gmail.com>
2017-07-25 20:31:15 -0400

    tweak and fix comparison exprdeparser

--- src/main/java/edu/umich/tbnalir/sql/ComparisonRemovalExprDeParser.java
+++ src/main/java/edu/umich/tbnalir/sql/ComparisonRemovalExprDeParser.java
@@ -1,17 +1,33 @@
 package edu.umich.tbnalir.sql;
 
 import edu.umich.tbnalir.util.Constants;
+import net.sf.jsqlparser.expression.*;
 import net.sf.jsqlparser.expression.operators.relational.*;
+
 
 /**
  * Created by cjbaik on 6/20/17.
  */
 public class ComparisonRemovalExprDeParser extends ConstantRemovalExprDeParser {
-    public static String removeComparisonOperator(ComparisonOperator cmp) {
+    public static String removeComparisonOperator(Expression expr) {
         StringBuilder sb = new StringBuilder();
-        sb.append(ConstantRemovalExprDeParser.removeConstantsFromExpr(cmp.getLeftExpression()));
-        sb.append(" " + Constants.CMP + " ");
-        sb.append(ConstantRemovalExprDeParser.removeConstantsFromExpr(cmp.getRightExpression()));
+        if (expr instanceof BinaryExpression) {
+            BinaryExpression binary = (BinaryExpression) expr;
+            sb.append(removeComparisonOperator(binary.getLeftExpression()));
+            sb.append(" " + Constants.CMP + " ");
+            sb.append(removeComparisonOperator(binary.getRightExpression()));
+        } else if (expr instanceof Parenthesis) {
+            sb.append('(');
+            sb.append(removeComparisonOperator(((Parenthesis) expr).getExpression()));
+            sb.append(')');
+        } else if (expr instanceof DoubleValue || expr instanceof HexValue || expr instanceof LongValue
+                || expr instanceof StringValue || expr instanceof DateValue || expr instanceof TimestampValue
+                || expr instanceof TimeValue || expr instanceof DateTimeLiteralExpression
+                || expr instanceof SignedExpression || expr instanceof Function) {
+            sb.append(ConstantRemovalExprDeParser.removeConstantsFromExpr(expr));
+        } else {
+            sb.append(expr.toString());
+        }
         return sb.toString();
     }
 
@@ -43,5 +59,12 @@ public class ComparisonRemovalExprDeParser extends ConstantRemovalExprDeParser {
     @Override
     public void visit(NotEqualsTo notEqualsTo) {
         this.getBuffer().append(removeComparisonOperator(notEqualsTo));
+    }
+
+    @Override
+    public void visit(Parenthesis parenthesis) {
+        this.getBuffer().append('(');
+        this.getBuffer().append(removeComparisonOperator(parenthesis.getExpression()));
+        this.getBuffer().append(')');
     }
 }
