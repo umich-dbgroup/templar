4ded5445b9516e83a0caf736db33dc508ef3f13e
Chris Baik <chrisjbaik@gmail.com>
2018-03-14 16:14:57 -0400

    library modifications, add baseline for SQLizer

--- src/main/java/edu/umich/templar/baseline/SQLizer.java
+++ src/main/java/edu/umich/templar/baseline/SQLizer.java
@@ -1,4 +1,7 @@
 package edu.umich.templar.baseline;
 
 public class SQLizer {
+    public static void main(String[] args) {
+        // Input: query ID, fragment type (proj, sel, relation, etc.)
+    }
 }
