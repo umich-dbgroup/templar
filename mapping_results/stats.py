3b8e66b95b6e5edd27fd5760834739e17317b7a6
Chris Baik <chrisjbaik@gmail.com>
2018-03-23 11:31:31 -0400

    add stats script for templar result parsing

---
+++ mapping_results/stats.py
@@ -0,0 +1,48 @@
+import argparse
+import re
+
+def analyze_file(filename):
+    total_frags_correct = 0
+    total_frags = 0
+    total_queries = 0
+
+    with open(filename) as f:
+        phrases = {}
+        for line in f:
+            if "::" in line:
+                phrase, result = line.split("::")
+                answers_str, guess_str = result.split(':')
+
+                if phrase not in phrases:
+                    phrases[phrase] = True
+
+                answers = []
+                for ans in answers_str.split(","):
+                    answers.append(ans.strip())
+
+                guess = guess_str.split('(')[0].strip()
+                phrases[phrase] = phrases[phrase] and (guess in answers)
+            # end of query
+            if line.startswith('== RESULT'):
+                for phrase, result in phrases.iteritems():
+                    if result:
+                        total_frags_correct += 1
+                    total_frags += 1
+                total_queries += 1
+                phrases = {}
+
+    print '==== FINAL STATS ===='
+    print 'Correct frags: ' + str(total_frags_correct)
+    print 'Total frags: ' + str(total_frags)
+    print 'Total queries: ' + str(total_queries)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+    args = parser.parse_args()
+
+    analyze_file(args.filename)
+
+
+if __name__ == '__main__':
+    main()
