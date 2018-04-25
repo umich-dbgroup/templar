27c0de47ece884e731f30a30bf8b57c11f6fc5d5
Chris Baik <chrisjbaik@gmail.com>
2018-04-25 16:48:54 -0400

    update with NaLIR+ results

--- src/main/java/edu/umich/templar/main/TemplarCV.java
+++ src/main/java/edu/umich/templar/main/TemplarCV.java
@@ -22,7 +22,7 @@ import java.util.*;
 public class TemplarCV {
     public static void main(String[] args) {
         if (args.length < 5) {
-            System.out.println("Usage: TemplarCV <testset> <frag/nlsql> <full/no_const/no_const_op> <log %> <type-oracle>");
+            System.out.println("Usage: TemplarCV <testset> <frag/nlsql> <full/no_const/no_const_op> <log %> <type-oracle> <include-join>");
             System.exit(1);
         }
         String dbName = args[0];
